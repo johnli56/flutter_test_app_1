351b11511edeb8afd0e7856632a2b0e7b747bf19
johnli56 <li.johnny34@gmail.com>
2018-12-24 02:40:35 -0800

    Made the Favorite button go to a new screen when pressed, Created 3 buttons on that new screen. First button goes to the new screen again (creates more copies of the same screen) second and third button just go back to the homepage.

--- lib/main.dart
+++ lib/main.dart
@@ -11,63 +11,9 @@ void main() {
   runApp(MyApp());
 }
 
-class FavoriteWidget extends StatefulWidget {
-  @override
-  _FavoriteWidgetState createState() => _FavoriteWidgetState();
-}
-
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = true;
-  int _favoriteCount = 41;
-
-  void _toggleFavorite() {
-    setState(() {
-      // If the lake is currently favorited, unfavorite it.
-      if (_isFavorited) {
-        _favoriteCount -= 1;
-        _isFavorited = false;
-        // Otherwise, favorite it.
-      } else {
-        _favoriteCount += 1;
-        _isFavorited = true;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          padding: EdgeInsets.all(0.0),
-          child: IconButton(
-            icon: (_isFavorited
-                ? Icon(Icons.star)
-                : Icon(Icons.star_border)),
-            color: Colors.red[500],
-            onPressed: _toggleFavorite,
-          ),
-        ),
-        //Tip: Placing the Text in a SizedBox and setting its width prevents a discernible “jump” when the text changes between the values of 40 and 41—this would otherwise occur because those values have different widths.
-        SizedBox(
-          width: 28.0,
-          child: InkWell(
-            onTap: _toggleFavorite,
-            child: Text('$_favoriteCount',
-                style: TextStyle(fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic)),
-          ),
-
-        ),
-      ],
-    );
-  }
-}
-
+//Home page
 class MyApp extends StatelessWidget {
   @override
-
 
   //First Title section
   Widget build(BuildContext context) {
@@ -97,7 +43,6 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-
           FavoriteWidget(),
         ],
       ),
@@ -131,6 +76,28 @@ class MyApp extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
+          Flexible(
+            flex: 1,
+            child: RaisedButton.icon(
+              onPressed: null,
+              /*() { Attempt to make the call button a button to navigate
+                  This doesn't work because MyApp is a parent of materialapp
+                  and navigation requires a materialapp to navigate.
+                  Solution: create a new class that extends or is not a part of MyApp
+                  and call navigator.push from there
+                  Done with the star/favorite button currently
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => JohnnyHomeScreen()),
+                );
+              },*/
+              icon: const Icon(Icons.cast, size: 18.0),
+              label: const Text(
+                'Cast',
+                semanticsLabel: 'RAISED BUTTON 2',
+              ),
+            ),
+          ),
           buildButtonColumn(Icons.call, 'CALL'),
           buildButtonColumn(Icons.near_me, 'ROUTE'),
           buildButtonColumn(Icons.share, 'SHARE'),
@@ -148,7 +115,6 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
         softWrap: true,
       ),
     );
-
 
     return MaterialApp(
       title: 'Flutter Demo',
@@ -173,187 +139,107 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
     );
   }
 }
-/*
-class MyApp extends StatelessWidget {
+
+//Favorite star widget on the main home page
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = false;
+  int _favoriteCount = 41;
+
+  void _toggleFavorite() {
+    setState(() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => JohnnyHomeScreen()),
+      );
+      // If the lake is currently favorited, unfavorite it.
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+        // Otherwise, favorite it.
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    // The titleSection is a container containing the title row
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      //32 pixels padding on all sides
-      //A Row is a widget used to display child widgets in a horizontal manner. The Row widget does not scroll.
-      child: Row(
-        children: [
-          //Expanded widget stretches across the column to use all remaining free space in the row
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
-                ),
-              ], //children bracket
-            ),
-          ),
-          Icon(
-            Icons.alarm,
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0.0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
             color: Colors.red[500],
+            onPressed: _toggleFavorite,
           ),
-          Text('41'),
-          Text('31'),
-        ],
-      ),
-    ),
-  };
+        ),
+        //Tip: Placing the Text in a SizedBox and setting its width prevents a discernible “jump” when the text changes between the values of 40 and 41—this would otherwise occur because those values have different widths.
+        SizedBox(
+          width: 28.0,
+          child: InkWell(
+            onTap: _toggleFavorite,
+            child: Text('$_favoriteCount',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontStyle: FontStyle.italic)),
+          ),
+        ),
+      ],
+    );
+  }
+}
 
-}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*class MyApp extends StatelessWidget {
+class JohnnyHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32.0),
-      child: Row(
-        children: [
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: Text(
-                    'Oeschinen Lake Campground',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Johnny's Screen"),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => JohnnyHomeScreen()),
+                    );
+                  },
+                  child: Text('Fuck'),
                 ),
-                Text(
-                  'Kandersteg, Switzerland',
-                  style: TextStyle(
-                    color: Colors.grey[500],
-                  ),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Shit'),
                 ),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Up'),
+                ),
+
+
               ],
             ),
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
-        ],
-      ),
-    );
 
-    Column buildButtonColumn(IconData icon, String label) {
-      Color color = Theme.of(context).primaryColor;
-
-      return Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(icon, color: color),
-          Container(
-            margin: const EdgeInsets.only(top: 8.0),
-            child: Text(
-              label,
-              style: TextStyle(
-                fontSize: 12.0,
-                fontWeight: FontWeight.w400,
-                color: color,
-              ),
-            ),
-          ),
-        ],
-      );
-    }
-
-    Widget buttonSection = Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          buildButtonColumn(Icons.call, 'CALL'),
-          buildButtonColumn(Icons.near_me, 'ROUTE'),
-          buildButtonColumn(Icons.share, 'SHARE'),
-        ],
-      ),
-    );
-
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32.0),
-      child: Text(
-        '''
-Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
-        ''',
-        softWrap: true,
-      ),
-    );
-
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Top Lakes'),
-        ),
-        body: ListView(
-          children: [
-            Image.asset(
-              'images/lake.jpg',
-              width: 600.0,
-              height: 240.0,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection,
-          ],
         ),
       ),
+
     );
   }
-*/
+}
